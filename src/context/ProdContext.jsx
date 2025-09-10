ad7a9dfe3208bf7b7fbb23a95eddb265e31132fb
jangkarbumi <wahyuuajigtn13@gmail.com>
2025-09-10 15:01:59 +0700

    Modify add to cart to exactly  only one item for unique item

--- src/context/ProdContext.jsx
+++ src/context/ProdContext.jsx
@@ -3,51 +3,34 @@ import { createContext, useState } from "react";
 export const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
-  const [cartitems, setCartItems] = useState([]);
+ const [cartitems, setCartItems] = useState([]);
 
-  const addToCart = (product) => {
-    setCartItems((prevItems) => {
-      const existingItem = prevItems.find((item) => item.id === product.id);
-      if (existingItem) {
-        return prevItems.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      } else {
+     const addToCart = (product) => {
+         setCartItems((prevItems) => {
+            const existingItem = prevItems.find((item) => item.id === product.id);
+            if (!existingItem) {
+              return [...prevItems, { ...product, quantity: 1 }];
+            } else {
+              return prevItems;
+            }
+         });
+     };
 
-        return [...prevItems, { ...product, quantity: 1 }];
-      }
-    });
-  };
+     const removeCart = (product) => {
+         setCartItems((prevItems) => {
+            return prevItems.filter((item) => item.id !== product.id);
+         });
+     };
 
-  const removeCart = (product) => {
-    setCartItems((prevItems) => {
-      const existingItem = prevItems.find((item) => item.id === product.id);
+     const contextValue = {
+         cartitems,
+         addToCart,
+         removeCart,
+     };
 
-      if (existingItem && existingItem.quantity > 1) {
-
-        return prevItems.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity - 1 }
-            : item
-        );
-      } else {
-        return prevItems.filter((item) => item.id !== product.id);
-      }
-    });
-  };
-
-
-  const contextValue = {
-    cartitems,
-    addToCart,
-    removeCart,
-  };
-
-  return (
-    <ProductContext.Provider value={contextValue}>
-      {children}
-    </ProductContext.Provider>
-  );
+     return (
+         <ProductContext.Provider value={contextValue}>
+            {children}
+         </ProductContext.Provider>
+     );
 };
